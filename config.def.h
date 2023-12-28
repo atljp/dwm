f1089f5cfe30d6a85f849c9187f58e684f562b16
atljp <atljp@t-online.de>
2023-12-28 17:05:31 +0100

    added vanitygaps patch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -37,16 +42,19 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-/*	{ "TTT",      bstack }, */
-/* 	{ "[@]",      spiral }, */
-/* 	{ "[\\]",     dwindle }, */
-/*	{ "[D]",      deck }, */
+	{ "TTT",      bstack },
+ 	{ "[@]",      spiral },
+ 	{ "[\\]",     dwindle },
+	{ "[D]",      deck },
 	{ "[M]",      monocle },
-/*	{ "|M|",      centeredmaster }, */
-/*	{ ">M>",      centeredfloatingmaster }, */
+	{ "|M|",      centeredmaster },
+/	{ ">M>",      centeredfloatingmaster },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,	      NULL },
 
@@ -84,14 +92,18 @@ static const Key keys[] = {
 	{ MODKEY,  	                XK_q,      killclient,     {0} },
 	{ MODKEY,	                XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} }, /* tile */
-/*	{ MODKEY|ShiftMask,		XK_t,	   setlayout,	   {.v = &layouts[1]} }, */ /* bstack */ 
-/*	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[2]} }, */ /* spiral */
-/*	{ MODKEY|ShiftMask,             XK_z,      setlayout,      {.v = &layouts[3]} }, */ /* dwindle */
-/*	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[4]} }, */ /* deck */
+	{ MODKEY|ShiftMask,		XK_t,	   setlayout,	   {.v = &layouts[1]} }, /* bstack */ 
+	{ MODKEY,                       XK_z,      setlayout,      {.v = &layouts[2]} }, /* spiral */
+	{ MODKEY|ShiftMask,             XK_z,      setlayout,      {.v = &layouts[3]} }, /* dwindle */
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[4]} }, /* deck */
 	{ MODKEY|ShiftMask,             XK_u,      setlayout,      {.v = &layouts[1]} }, /* monocle */
-/*	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[6]} }, */ /* centeredmaster */
-/*	{ MODKEY|ShiftMask,             XK_i,      setlayout,      {.v = &layouts[7]} }, */ /* centeredfloatingmaster */
+	{ MODKEY,                       XK_i,      setlayout,      {.v = &layouts[6]} }, /* centeredmaster */
+	{ MODKEY|ShiftMask,             XK_i,      setlayout,      {.v = &layouts[7]} }, /* centeredfloatingmaster */
 	{ MODKEY|ShiftMask,		XK_f,	   setlayout,      {.v = &layouts[2]} }, /* floating */
+	{ MODKEY,	                XK_y,      incrgaps,       {.i = +3 } },
+	{ MODKEY, 		        XK_x,      incrgaps,       {.i = -3 } },
+	{ MODKEY,	                XK_a,      togglegaps,     {0} },
+	{ MODKEY|ShiftMask,	        XK_a,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
