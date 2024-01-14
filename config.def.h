c0dba7163cde7bf376f9a22876391880dfc3d6f2
altjp <atljp@t-online.de>
2024-01-14 13:25:14 +0100

    lutris float rule

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static       int smartgaps          = 0;        /* 1 means no outer gap when the
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 /* static const Bool viewontag         = True; */    /* Switch view on tag switch */
-static char *fonts[]          =	    { "monospace:size=10" };
+static char *fonts[]          =	    { "martian mono:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -47,6 +47,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "lutris",  NULL,     NULL,           0,         1,          0,           1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
